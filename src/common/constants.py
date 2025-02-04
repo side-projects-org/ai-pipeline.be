143ba3ebe1c24b281be721490bc3f975ccbc94a5
0woodev <0woodev@gmail.com>
2025-02-04 22:55:48 +0900

    FEAT constants 환경변수 읽어오는 함수 구현, 테스트

---
+++ src/common/constants.py
@@ -0,0 +1,30 @@
+import os
+import logging
+
+
+logger = logging.getLogger("src.common.constants")
+
+
+def get_env(env_name, filename, default='not-assigned'):
+    env = os.environ.get(env_name, default)
+
+    if env == default:
+        file = os.path.join(os.path.dirname(__file__), filename)
+        file = os.path.abspath(file)
+        # 파일이 있는 경우에만 읽어서 업데이트하도록 수정
+        if os.path.exists(file):
+            f = open(file, "r")
+            env = f.read().strip().strip("\"")
+            f.close()
+        else:
+            logger.error(f"ENV '{env_name}' not setting. Check the file or environment variable.\n"
+                         f"Searching File Path: {file}")
+
+    return env
+
+
+class BaseConfig:
+    AWS_REGION = 'ap-northeast-2'
+    PROJECT_NAME = get_env('PROJECT_NAME', '../../project_env/live/project_name.txt')
+    STAGE_NAME = get_env('STAGE_NAME', '../../project_env/live/stage_name.txt')
+    FCM_KEY = get_env('FCM_KEY', '../../project_env/live/fcm_key.txt')
---
+++ src/common/test_constants.py
@@ -0,0 +1,36 @@
+import os
+import unittest
+
+from unittest import TestCase
+
+from src.common.constants import get_env
+
+
+class Test(TestCase):
+
+    def test_get_env_no_file(self):
+        # there is no file in that path
+        default_value = "qwert"
+        actual = get_env('PROJECT_NAME', '../../project_env/project_name.txt', default_value)
+
+        self.assertEqual(actual, default_value)
+
+    # @unittest.skip
+    def test_get_env_with_file(self):
+        # this test case depends on the file project_name.txt
+        # so, it will fail if the file changes
+        actual = get_env('PROJECT_NAME', '../../project_env/sample/project_name.txt')
+
+        self.assertEqual(actual, 'project_name')
+
+    def test_get_local_env(self):
+        # Set environment variable
+        # given
+        expect_env_value = 'value'
+        os.environ['PROJECT_NAME'] = expect_env_value
+
+        # when
+        actual = get_env('PROJECT_NAME', "unnecessary")
+
+        # then
+        self.assertEqual(actual, expect_env_value)
