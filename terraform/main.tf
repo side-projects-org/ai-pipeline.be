100a1d9b3ee6035c898c258f96ca3643f421ec97
0woodev <0woodev@gmail.com>
2025-05-10 21:38:22 +0900

    INFRA - Lambda IAM Role 에 Policy 적용

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,7 +14,7 @@ resource "aws_s3_object" "s3_for_layer_library_common" {
     key = "build/${var.projectName}/layer/layer_library_common/layer.zip"
     source = "../src/layer/layer_library_common/layer.zip"
 
-    source_hash = filesha256("../src/layer/layer_library_common/build.zip")
+    source_hash = filesha256("../src/layer/layer_library_common/layer.zip")
 
     depends_on = [aws_s3_bucket.source_build_bucket]
 }
@@ -45,7 +45,7 @@ resource "aws_s3_object" "s3_for_layer_common" {
     key = "build/${var.projectName}/common/layer.zip"
     source = "../src/common/layer.zip"
 
-    source_hash = filesha256("../src/common/build.zip")
+    source_hash = filesha256("../src/common/layer.zip")
 
     depends_on = [aws_s3_bucket.source_build_bucket]
 }
@@ -79,6 +79,7 @@ resource "aws_s3_object" "s3_for_lambda_api_sample_api_get_sample_model_by_key" 
     depends_on = [aws_s3_bucket.source_build_bucket]
 }
 
+
 data "aws_iam_policy_document" "iam_for_lambda_role" {
   statement {
     actions = ["sts:AssumeRole"]
@@ -86,21 +87,147 @@ data "aws_iam_policy_document" "iam_for_lambda_role" {
       identifiers = [
         "lambda.amazonaws.com",
         "sqs.amazonaws.com",
-        "rds.amazonaws.com",
-        "dynamodb.amazonaws.com",
+        "rds.amazonaws.com"
       ]
       type = "Service"
     }
     effect = "Allow"
   }
 }
+data "aws_iam_policy_document" "iam_for_lambda_policy" {
+  version = "2012-10-17"
+  statement {
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:DescribeLogGroups",
+      "logs:DescribeLogStreams",
+      "logs:PutLogEvents",
+      "logs:GetLogEvents",
+      "logs:FilterLogEvents"
+    ]
+    resources = ["arn:aws:logs:*:*:*"]
+    effect    = "Allow"
+  }
+
+  statement {
+    actions = [
+      "dynamodb:DescribeReservedCapacity*",
+      "dynamodb:DescribeLimits",
+
+      "dynamodb:ListContributorInsights",
+      "dynamodb:ListExports",
+      "dynamodb:ListGlobalTables",
+      "dynamodb:ListTables",
+
+      "dynamodb:ListStreams",
+      "dynamodb:ListBackups"
+    ]
+    resources = ["*"]
+    effect    = "Allow"
+    sid       = "ListAndDescribeNoLimit"
+  }
+
+  statement {
+    actions = [
+      "dynamodb:BatchGet*",
+      "dynamodb:Get*",
+
+      "dynamodb:DescribeStream",
+      "dynamodb:DescribeTable",
+      "dynamodb:DescribeTimeToLive",
+      "dynamodb:Query",
+      "dynamodb:Scan"
+    ]
+    resources = [
+      "arn:aws:dynamodb:${var.awsRegion}:*:table/*",
+      "arn:aws:dynamodb:${var.awsRegion}:*:*/index/*"
+    ]
+    effect = "Allow"
+    sid    = "ListAndDescribe"
+  }
+
+  statement {
+    actions = [
+      "dynamodb:BatchWrite*",
+      "dynamodb:Delete*",
+      "dynamodb:Update*",
+
+      "dynamodb:PutItem"
+    ]
+    resources = [
+      "arn:aws:dynamodb:${var.awsRegion}:*:table/*",
+      "arn:aws:dynamodb:${var.awsRegion}:*:*/index/*"
+    ]
+    effect = "Allow"
+    sid    = "SpecificTable"
+  }
+
+  statement {
+    actions = [
+      "tag:GetTagKeys",
+      "tag:GetTagValues"
+    ]
+    resources = ["*"]
+    effect    = "Allow"
+  }
+
+#  statement {
+#    effect  = "Allow"
+#    actions = [
+#      "s3:GetBucket*",
+#      "s3:GetObject*"
+#    ]
+#    resources = [
+#      "arn:aws:s3:::secretkey-for-oauth",
+#      "arn:aws:s3:::secretkey-for-oauth/*"
+#    ]
+#  }
+
+  statement {
+    effect  = "Allow"
+    actions = [
+      "sqs:GetQueueUrl",
+      "sqs:SendMessage",
+#      "sqs:ReceiveMessage",
+#      "sqs:DeleteMessage",
+      "sqs:GetQueueAttributes"
+    ]
+    resources = ["arn:aws:sqs:*:*:*"]
+  }
+
+  statement {
+    effect  = "Allow"
+    actions = [
+      "sqs:ReceiveMessage",
+      "sqs:DeleteMessage",
+      "sqs:GetQueueAttributes"
+    ]
+    resources = ["arn:aws:sqs:*:*:*"]
+  }
+}
+
 
 resource "aws_iam_role" "default_iam_role" {
     name = "${var.projectName}_default_iam_role"
 
-
     assume_role_policy = data.aws_iam_policy_document.iam_for_lambda_role.json
 }
+
+# See also the following AWS managed policy: AWSLambdaBasicExecutionRole
+resource "aws_iam_policy" "default_iam_policy" {
+  name        = "${var.projectName}_default_iam_policy"
+  path        = "/"
+  description = "IAM policy for logging from a lambda"
+
+  policy = data.aws_iam_policy_document.iam_for_lambda_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "default_iam_role_policy_attachment" {
+  role       = aws_iam_role.default_iam_role.name
+  policy_arn = aws_iam_policy.default_iam_policy.arn
+}
+
 
 resource "aws_lambda_function" "lambda_for_api_sample_api_get_sample_model_by_key" {
     # count = fileexists("../src/layer/layer_library_common/build.zip") ? 1 : 0
