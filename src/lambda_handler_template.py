1dd739f2ecd1fab1a3c923ae6136e8ed55ccaf25
이선재 <sj06152@naver.com>
2025-05-12 23:34:29 +0900

    FEAT - API 폴더 구조 만드는 makefile 추가

---
+++ src/lambda_handler_template.py
@@ -0,0 +1,27 @@
+from http import HTTPStatus
+
+from pynamodb.exceptions import DoesNotExist
+
+from common import APIException, ErrorCode
+from common.pynamo_util import model_to_dict
+from common.dynamodb.model.SampleModel import SampleModel
+
+from common.awslambda.response_handler import ResponseHandler
+
+
+@ResponseHandler.api
+def lambda_handler(event, context):
+    # QUERY PARAMETER 에서 key 를 꺼내온다.
+    if 'queryStringParameters' not in event or 'key' not in event['queryStringParameters']:
+        exc = Exception('there is no required parameters')
+        setattr(exc, 'status_code', HTTPStatus.BAD_REQUEST)
+        raise exc
+
+    key = event['queryStringParameters']['key']
+
+    try:
+        sample = SampleModel.get(key)
+    except DoesNotExist as e:
+        raise APIException(ErrorCode.DYNAMO_ITEM_NOT_FOUND, key=key)
+
+    return model_to_dict(sample)
