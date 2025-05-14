ca3ba00333dd3e299581ee7535af708fbf806920
leessunj <sj06152@gmail.com>
2025-05-15 08:27:59 +0900

    feat - cors 해결

--- terraform/sample_terraform/apigateway.tf
+++ terraform/sample_terraform/apigateway.tf
@@ -1,6 +1,12 @@
 resource "aws_apigatewayv2_api" "http_api" {
   name          = "${var.projectName}-${var.stage}"
   protocol_type = "HTTP"
+  cors_configuration {
+    allow_origins = ["https://main.d2x72b47hczm52.amplifyapp.com", "http://localhost"]
+    allow_methods = ["*"]
+    allow_headers = ["Content-Type", "Authorization"]
+    max_age       = 3600
+  }
 }
 
 resource "aws_apigatewayv2_stage" "default" {
