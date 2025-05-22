84d01e5fc3744ee199165d3d38fa29b46ca6e211
0woodev <0woodev@gmail.com>
2025-05-22 15:15:08 +0900

    TINY - model_to_dict 은 동작하나, 코드 퀄리티 또한 별로인듯 (to_simple_dict 로 변경함)

--- src/common/pynamo_util.py
+++ src/common/pynamo_util.py
@@ -5,11 +5,11 @@ from pynamodb.attributes import MapAttribute, DynamicMapAttribute, TTLAttribute
 from pynamodb.models import Model
 
 
-def _model_to_dict(model: Model):
+def _model_to_dict(model):
     for attr_name in model.get_attributes().keys():
         attr = getattr(model, attr_name)
         if isinstance(attr, MapAttribute) or isinstance(attr, DynamicMapAttribute):
-            attr = attr.as_dict()
+            attr = dict(model_to_dict(attr))
         elif isinstance(attr, list):
             attr = [dict(model_to_dict(item)) if isinstance(item, MapAttribute) or isinstance(item, DynamicMapAttribute) else item for item in attr]
         yield attr_name, attr
