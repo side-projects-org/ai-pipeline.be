9af1dc6a2092e419a9e78bc54ff19ee50b99ac43
leessunj <sj06152@gmail.com>
2025-05-25 23:12:20 +0900

    feat - prompt 생성 시 LATEST 업데이트, Version 이름 validate

--- src/api/prompt/api_put_prompt_model/api_put_prompt_model.py
+++ src/api/prompt/api_put_prompt_model/api_put_prompt_model.py
@@ -1,17 +1,12 @@
-import json
 import uuid
 from datetime import datetime, timezone
 
 from common import APIException, ErrorCode
-from common.pynamo_util import model_to_dict
+from common.awslambda.request_util import get_body
 
-from common.awslambda.response_handler import ResponseHandler
+from common.awslambda.response_handler import ResponseHandler, logger
 
-from common.dynamodb.model import Prompt
-
-from common.dynamodb.model import AIRequestParamsAttribute
-
-from common.dynamodb.model import AIMessageAttribute
+from common.dynamodb.model import Prompt, AIRequestParamsAttribute, AIMessageAttribute
 
 # prompt_name : 프롬프트별 식별값 : string (url 에서 허용되는지 검증)
 # version : 프롬프트별 버젼 (Default 는 UTC Current) : string (url 에서 허용되는지 검증)
@@ -22,42 +17,88 @@ from common.dynamodb.model import AIMessageAttribute
 # temperature : 0 ~ 1 : Number
 # max_completion_tokens : Number
 
-REQUIRED_KEYS = {"prompt_name", "model", "messages", "temperature", "max_completion_tokens"}
+REQUIRED_KEYS = {"prompt_name", "model", "messages", "temperature", "max_completion_tokens", "version"}
+
+
 def validate_required_keys(data: dict):
     missing_keys = REQUIRED_KEYS - data.keys()
     if missing_keys:
         raise ValueError(f"Missing required keys: {', '.join(missing_keys)}")
 
+
+LATEST_VERSION = "LATEST"
+RELEASE_VERSION = "RELEASE"
+PROHIBITED_VERSION_NAME = {LATEST_VERSION, RELEASE_VERSION}
+
+
+def validate_version_name(version: str, prompt_name: str):
+    if version in PROHIBITED_VERSION_NAME:
+        raise APIException(ErrorCode.INVALID_PARAMETER, param=f"Not Allowed Version Name: {version}")
+    # DUPLICATE VERSION ERROR
+    items = list(Prompt.prompt_name__version__index.query(prompt_name, Prompt.version == version))
+    if items:
+        raise APIException(ErrorCode.INVALID_PARAMETER,
+                           param=f"Duplicated Version Name: {version} (for prompt: {prompt_name})")
+
+
 @ResponseHandler.api
 def lambda_handler(event, context):
-    try:
-        body = json.loads(event.get("body", "{}"))
-        validate_required_keys(body)
-        key=uuid.uuid4().__str__()
-        version =datetime.now(timezone.utc).isoformat() if "version" not in body else body["version"]
-        # TODO messages 형태도 미리 검증하면 좋을 듯
-        prompt = Prompt(
-            key=key,
-            prompt_name=body["prompt_name"],
-            version=version,
-            params=AIRequestParamsAttribute(
-                model=body["model"],
-                messages=[AIMessageAttribute(**msg) for msg in body["messages"]],
-                temperature=body["temperature"],
-                max_completion_tokens=body["max_completion_tokens"],
-                response_format="text",
-            ),
-            created_at=datetime.now(timezone.utc),
-            updated_at=datetime.now(timezone.utc),
+    body = get_body(event)
+
+    validate_required_keys(body)
+    # version = datetime.now(timezone.utc).isoformat() if "version" not in body else body["version"]
+    version = body["version"]
+    prompt_name = body["prompt_name"]
+    validate_version_name(version, prompt_name)
+
+    key = uuid.uuid4().__str__()
+    now = datetime.now(timezone.utc)
+    # TODO messages 형태도 미리 검증하면 좋을 듯
+    prompt = Prompt(
+        key=key,
+        prompt_name=prompt_name,
+        version=version,
+        params=AIRequestParamsAttribute(
+            model=body["model"],
+            messages=[AIMessageAttribute(**msg) for msg in body["messages"]],
+            temperature=body["temperature"],
+            max_completion_tokens=body["max_completion_tokens"],
+            response_format="text",
+        ),
+        created_at=now,
+        updated_at=now,
+    )
+    prompt.save()
+
+    put_latest_model(prompt)
+
+    data = prompt.to_simple_dict()
+    return data
+
+
+def put_latest_model(prompt: Prompt):
+    items = list(Prompt.version__prompt_name__index.query(LATEST_VERSION, Prompt.prompt_name == prompt.prompt_name))
+
+    if items:
+        # 기존 데이터 업데이트
+        latest_prompt = items[0]
+        update_data = {
+            "applied_version": prompt.version,
+            "params": prompt.params,
+            "updated_at": prompt.created_at,
+        }
+        for key, val in update_data.items():
+            setattr(latest_prompt, key, val)
+        latest_prompt.save()
+    else:
+        # 새로 생성
+        latest_prompt = Prompt(
+            key=str(uuid.uuid4()),
+            prompt_name=prompt.prompt_name,
+            version=LATEST_VERSION,
+            applied_version=prompt.version,
+            params=prompt.params,
+            created_at=prompt.created_at,
+            updated_at=prompt.updated_at,
         )
-        prompt.save()
-
-        data = prompt.to_simple_dict()
-        print(f'{data}')
-        return data
-    except ValueError as valueError:
-        raise APIException(ErrorCode.PARAMETER_NOT_FOUND, param=str(valueError))
-    except Exception as e:
-        raise APIException(ErrorCode.INTERNAL_ERROR, detail=str(e))
-
-
+        latest_prompt.save()
