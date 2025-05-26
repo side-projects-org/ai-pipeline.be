040c2c2fb2ef3799f23d1041dab261e1a67235a7
Youngwoo <86946740+0woodev@users.noreply.github.com>
2025-05-26 23:07:04 +0900

    18 chatgpt api wrapping (#26)
    * FEAT - OPEN_AI_KEY 설정 및 ChatGPT 에 요청하는 람다 생성 (리팩토링 필요)
    
    * FEAT - openai library 사용하는 v2 메서드 제작

---
+++ src/api/ai/api_post_new_ai_response/api_post_new_ai_response.py
@@ -0,0 +1,39 @@
+import requests
+
+from common import BaseConfig
+from common.awslambda.request_util import get_body
+from common.awslambda.response_handler import ResponseHandler
+
+@ResponseHandler.api
+def lambda_handler(event, context):
+    # body 에서 가져온 messages, temperature, max_completion_tokens, model 을 사용하여
+    # ChatGPT API 를 호출하여 응답을 받아온다.
+    body = get_body(event)
+
+    header = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {BaseConfig.OPEN_AI_KEY}'
+    }
+
+    response = requests.post(
+        'https://api.openai.com/v1/chat/completions',
+        headers=header,
+        json={
+            'model': body['model'],
+            'messages': body['messages'],
+            'temperature': body['temperature'],
+            'max_tokens': body['max_completion_tokens']
+        }
+    )
+
+    data = response.json()
+
+    return {
+        'used_params': {
+            'model': body['model'],
+            'messages': body['messages'],
+            'temperature': body['temperature'],
+            'max_tokens': body['max_completion_tokens']
+        },
+        'response': data
+    }
---
+++ src/api/ai/api_post_new_ai_response_v2/api_post_new_ai_response_v2.py
@@ -0,0 +1,29 @@
+from openai import OpenAI
+
+from common import BaseConfig
+
+from common.awslambda.request_util import get_body
+from common.awslambda.response_handler import ResponseHandler
+
+client = OpenAI(api_key=BaseConfig.OPEN_AI_KEY)
+
+@ResponseHandler.api
+def lambda_handler(event, context):
+    # body 에서 가져온 messages, temperature, max_completion_tokens, model 을 사용하여
+    # ChatGPT API 를 호출하여 응답을 받아온다.
+    body = get_body(event)
+
+    response = client.chat.completions.create(model=body['model'],
+    messages=body['messages'],
+    temperature=body['temperature'],
+    max_tokens=body['max_completion_tokens'])
+
+    return {
+        'used_params': {
+            'model': body['model'],
+            'messages': body['messages'],
+            'temperature': body['temperature'],
+            'max_tokens': body['max_completion_tokens']
+        },
+        'response': response.to_dict()
+    }
