e21ded4508b5c3d99b6319def57b7349b2d222c9
0woodev <0woodev@gmail.com>
2025-06-04 20:58:38 +0900

    Chore - Makefile make api 수정 (__init__.py 및 test code 도 생성하도록 변경)

---
+++ src/api/ai_response/api_get_ai_response_list/api_get_ai_response_list.py
@@ -0,0 +1,26 @@
+from http import HTTPStatus
+
+from pynamodb.exceptions import DoesNotExist
+
+from common import APIException, ErrorCode
+from common.dynamodb.model import M
+
+from common.awslambda.response_handler import ResponseHandler
+
+
+@ResponseHandler.api
+def lambda_handler(event, context):
+    # QUERY PARAMETER 에서 key 를 꺼내온다.
+    if 'queryStringParameters' not in event or 'key' not in event['queryStringParameters']:
+        exc = Exception('there is no required parameters')
+        setattr(exc, 'status_code', HTTPStatus.BAD_REQUEST)
+        raise exc
+
+    key = event['queryStringParameters']['key']
+
+    try:
+        sample = M.Sample.get(key)
+    except DoesNotExist as e:
+        raise APIException(ErrorCode.DYNAMO_ITEM_NOT_FOUND, key=key)
+
+    return sample.to_simple_dict()
