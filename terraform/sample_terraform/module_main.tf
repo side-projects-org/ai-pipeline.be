f220d22936fb4afbeff3373eec9fae8cfdf35795
0woodev <0woodev@gmail.com>
2025-06-08 21:52:25 +0900

    Infra - ai_response POST, GET, DELETE API 배포

--- terraform/sample_terraform/module_main.tf
+++ terraform/sample_terraform/module_main.tf
@@ -56,17 +56,17 @@ module "api_put_prompt_model" {
 }
 
 
-module "api_post_request_llm" {
+module "api_post_llm" {
   source = "../modules/api_endpoint"
 
   projectName = var.projectName
   stage       = var.stage
 
-  lambda_name     = "api_post_request_llm"
-  lambda_handler  = "api.ai.api_post_request_llm.api_post_request_llm.lambda_handler"
+  lambda_name     = "api_post_llm"
+  lambda_handler  = "api.ai.api_post_llm.api_post_llm.lambda_handler"
   lambda_role_arn = aws_iam_role.default_iam_role.arn
 
-  lambda_zip_path = "api/ai/api_post_request_llm/build.zip"
+  lambda_zip_path = "api/ai/api_post_llm/build.zip"
 
   lambda_layers = [
     aws_lambda_layer_version.layer_library_common_version.arn,
@@ -85,17 +85,17 @@ module "api_post_request_llm" {
   s3_source_bucket = aws_s3_bucket.source_build_bucket
 }
 
-module "api_post_request_llm_v2" {
+module "api_post_llm_v2" {
   source = "../modules/api_endpoint"
 
   projectName = var.projectName
   stage       = var.stage
 
-  lambda_name     = "api_post_request_llm_v2"
-  lambda_handler  = "api.ai.api_post_request_llm_v2.api_post_request_llm_v2.lambda_handler"
+  lambda_name     = "api_post_llm_v2"
+  lambda_handler  = "api.ai.api_post_llm_v2.api_post_llm_v2.lambda_handler"
   lambda_role_arn = aws_iam_role.default_iam_role.arn
 
-  lambda_zip_path = "api/ai/api_post_request_llm_v2/build.zip"
+  lambda_zip_path = "api/ai/api_post_llm_v2/build.zip"
 
   lambda_layers = [
     aws_lambda_layer_version.layer_library_common_version.arn,
@@ -111,5 +111,93 @@ module "api_post_request_llm_v2" {
   api_gateway_id   = aws_apigatewayv2_api.http_api.id
   api_gateway_arn  = aws_apigatewayv2_api.http_api.execution_arn
   route_key        = "POST /ai/v2"
+  s3_source_bucket = aws_s3_bucket.source_build_bucket
+}
+
+module "api_get_ai_response_list" {
+  source = "../modules/api_endpoint"
+
+  projectName = var.projectName
+  stage       = var.stage
+
+  lambda_name     = "api_get_ai_response_list"
+  lambda_handler  = "api.ai_response.api_get_ai_response_list.api_get_ai_response_list.lambda_handler"
+  lambda_role_arn = aws_iam_role.default_iam_role.arn
+
+  lambda_zip_path = "api/ai_response/api_get_ai_response_list/build.zip"
+
+  lambda_layers = [
+    aws_lambda_layer_version.layer_library_common_version.arn,
+    aws_lambda_layer_version.layer_common_version.arn,
+  ]
+
+  environment_variables = {
+    PROJECT_NAME = var.projectName
+    STAGE_NAME   = var.stage
+    OPEN_AI_KEY = var.openAIKey
+  }
+
+  api_gateway_id   = aws_apigatewayv2_api.http_api.id
+  api_gateway_arn  = aws_apigatewayv2_api.http_api.execution_arn
+  route_key        = "GET /ai-response"
+  s3_source_bucket = aws_s3_bucket.source_build_bucket
+}
+
+module "api_post_ai_response" {
+  source = "../modules/api_endpoint"
+
+  projectName = var.projectName
+  stage       = var.stage
+
+  lambda_name     = "api_post_ai_response"
+  lambda_handler  = "api.ai_response.api_post_ai_response.api_post_ai_response.lambda_handler"
+  lambda_role_arn = aws_iam_role.default_iam_role.arn
+
+  lambda_zip_path = "api/ai_response/api_post_ai_response/build.zip"
+
+  lambda_layers = [
+    aws_lambda_layer_version.layer_library_common_version.arn,
+    aws_lambda_layer_version.layer_common_version.arn,
+  ]
+
+  environment_variables = {
+    PROJECT_NAME = var.projectName
+    STAGE_NAME   = var.stage
+    OPEN_AI_KEY = var.openAIKey
+  }
+
+  api_gateway_id   = aws_apigatewayv2_api.http_api.id
+  api_gateway_arn  = aws_apigatewayv2_api.http_api.execution_arn
+  route_key        = "POST /ai-response"
+  s3_source_bucket = aws_s3_bucket.source_build_bucket
+}
+
+
+module "api_delete_ai_response" {
+  source = "../modules/api_endpoint"
+
+  projectName = var.projectName
+  stage       = var.stage
+
+  lambda_name     = "api_delete_ai_response"
+  lambda_handler  = "api.ai_response.api_delete_ai_response.api_delete_ai_response.lambda_handler"
+  lambda_role_arn = aws_iam_role.default_iam_role.arn
+
+  lambda_zip_path = "api/ai_response/api_delete_ai_response/build.zip"
+
+  lambda_layers = [
+    aws_lambda_layer_version.layer_library_common_version.arn,
+    aws_lambda_layer_version.layer_common_version.arn,
+  ]
+
+  environment_variables = {
+    PROJECT_NAME = var.projectName
+    STAGE_NAME   = var.stage
+    OPEN_AI_KEY = var.openAIKey
+  }
+
+  api_gateway_id   = aws_apigatewayv2_api.http_api.id
+  api_gateway_arn  = aws_apigatewayv2_api.http_api.execution_arn
+  route_key        = "DELETE /ai-response"
   s3_source_bucket = aws_s3_bucket.source_build_bucket
 }
